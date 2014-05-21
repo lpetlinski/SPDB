763d4c8b8a5bd1d200d205ad336cd7f0c9cb3aeb
Lukasz <lukaszpetlinski@gmail.com>
2014-05-21 22:34:27 +0200

    initial commit

---
+++ SPDB/BaseRepository.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPDB.DAL
+{
+    public abstract class BaseRepository
+    {
+        private MySqlConnection connection;
+
+        public MySqlConnection Connection
+        {
+            get
+            {
+                return this.connection;
+            }
+        }
+
+        public BaseRepository()
+        {
+            var connectionString = new StringBuilder();
+            connectionString.Append("SERVER=");
+            connectionString.Append(SPDB.DAL.Properties.Settings.Default.DbServer);
+            connectionString.Append(";DATABASE=");
+            connectionString.Append(SPDB.DAL.Properties.Settings.Default.DbDatabase);
+            connectionString.Append(";UID=");
+            connectionString.Append(SPDB.DAL.Properties.Settings.Default.DbUsername);
+            connectionString.Append(";PASSWORD=");
+            connectionString.Append(SPDB.DAL.Properties.Settings.Default.DbPassword);
+            this.connection = new MySqlConnection(connectionString.ToString());
+        }
+
+        protected bool OpenConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                return true;
+            }
+            catch (MySqlException exc)
+            {
+                switch (exc.Number)
+                {
+                    case 0:
+                        //TODO Cannot connect to server
+                        break;
+                    case 1045:
+                        //TODO invalid login or password
+                        break;
+                }
+                return false;
+            }
+        }
+
+        protected bool CloseConnection()
+        {
+            try
+            {
+                this.connection.Close();
+                return true;
+            }
+            catch(MySqlException exc)
+            {
+                //TODO handle exception
+                return false;
+            }
+        }
+    }
+}
---
+++ SPDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SPDB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SPDB")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7aade44a-74e5-4d0b-9e6d-9745b3be8c93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
